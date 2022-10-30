74aefc9ab292804271f5ab60f7f1304c6a014650
Brad Roehrig'git config --global user.email bradroehrig@yahoo.comgit config --global --listgit config --global user.name Brad <63978952+bradroehrig@users.noreply.github.com>
2022-10-30 16:18:10 -0500

    commit

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
+#from django.http import Http404
 
 def post_list(request):
     posts = Post.published.all()
@@ -9,10 +9,9 @@ def post_list(request):
                 {'posts': posts})
 
 def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("No post found.")
+    post = get_object_or_404(Post,
+                            id=id,
+                            status=Post.Status.PUBLISHED)
     
     return render(request,
                 'blog/post/detail.html',
